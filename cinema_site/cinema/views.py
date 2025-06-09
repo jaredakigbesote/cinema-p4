a9498476f140f06aa04a5d36f8503ddbf23d8fed
jaredakigbesote g <9bn46t32@students.codeinstitute.net>
2025-06-09 18:39:06 +0000

    Add movie_list_view to display all movies on homepage

--- cinema_site/cinema/views.py
+++ cinema_site/cinema/views.py
@@ -10,3 +10,10 @@ def movie_detail_view(request, pk):
         'movie': movie,
         'screenings': screenings,
     })
+
+from django.shortcuts import render
+from .models import Movie
+
+def movie_list_view(request):
+    movies = Movie.objects.all()
+    return render(request, 'cinema/movie_list.html', {'movies': movies})
