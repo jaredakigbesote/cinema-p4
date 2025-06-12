e3ad07a983c99f91d9266576e16bdb2f3c5f7c77
jaredakigbesote g <9bn46t32@students.codeinstitute.net>
2025-06-12 17:22:29 +0000

    Re-added login/logout functionality and fixed related issues

--- cinema_site/cinema/urls.py
+++ cinema_site/cinema/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.movie_list_view, name='movie_list'),
     path('movie/<int:pk>/', views.movie_detail_view, name='movie_detail'),
     path('book/<int:screening_id>/', views.book_screening_view, name='book_screening'),
-
+    path('login/', auth_views.LoginView.as_view(template_name='cinema/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='movie_list'), name='logout'),
+   
 ]
+
