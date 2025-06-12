c983538db317210936b1ba1313dd8f040c19b91a
jaredakigbesote g <9bn46t32@students.codeinstitute.net>
2025-06-12 19:00:12 +0000

    Fix conflicting model definition by removing duplicate Movie model from views.py and added cde to logout.html

--- cinema_site/cinema/views.py
+++ cinema_site/cinema/views.py
@@ -41,11 +41,6 @@ def book_screening_view(request, screening_id):
 def test_view(request):
     return HttpResponse("Login route works!")
 
-class Movie(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(upload_to='movie_images/', blank=True, null=True)
-
     def __str__(self):
         return self.title
 
