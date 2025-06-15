5623612458451b7d7f1427ee218259d8958bdc38
jaredakigbesote g <9bn46t32@students.codeinstitute.net>
2025-06-15 08:28:36 +0000

    fixd cloudinary

--- cinema/models.py
+++ cinema/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     duration = models.PositiveIntegerField(help_text="Duration in minutes")
     release_date = models.DateField()
-    image = models.ImageField(upload_to='movie_images/', null=True, blank=True)
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.title
